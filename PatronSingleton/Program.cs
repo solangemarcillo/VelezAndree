4f44643fa83b3d3bebad4698280209cb595658c5
rodri <rodri@LAPTOP-SN60J7OR>
2020-01-16 13:55:41 -0500

    hola

---
+++ PatronSingleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PatronSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui se llama a la clase  singlenton mas la varible  que es "Instancia" mas la variable 
+            //"Titulo" que esta en el constructor de la clases singlento
+            Console.WriteLine(Singleton.Instancia.Titulo);
+
+            Singleton.Instancia.Titulo = "Patrones de diseño";
+            Console.WriteLine(Singleton.Instancia.Titulo);
+
+            Singleton.Instancia.Titulo = "Patron Singleton";
+            Console.WriteLine(Singleton.Instancia.Titulo);
+        }
+    }
+}
---
+++ PatronSingleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatronSingleton
+{
+    public class Singleton
+    {
+        //Se crea una propiedad privada con un valor de null
+        private static Singleton instancia=null;
+
+        //Creas un metodo Estatico De la clase el cual tenfra un get y despues ahara un recorrido con el " if " el cual pregunta
+        // si instancia es igaul que null y el verificara en la variable instancia el cual tiene como valor el "new con el constructor dela clases"
+        public static Singleton Instancia
+        {
+            get 
+            {
+                if (instancia == null)
+                {
+                    instancia = new Singleton();
+                }
+                return instancia;
+            }
+            
+        }
+        public String Titulo = "";
+
+
+        //Este es el constructor con una variable que tiene como valor un mesanje de "Patrones"
+        public Singleton()
+        {
+            Titulo = "Patrones";
+        }
+    }
+}
